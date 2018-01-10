b581a1cdad7c43e1fbd6f04abdd64bbb9ad9bc1c
Gang Tao <gtao@splunk.com>
2018-01-09 16:00:00 -0800

    Add node code validator

--- src/static/js/components/nodeCodePanel.js
+++ src/static/js/components/nodeCodePanel.js
@@ -16,7 +16,7 @@ define(["util"], function(Util) {
         this._body.append("textarea").classed("form-control", true).attr("id", "NodeCodeEditor");
 
         var heading = root.select(".panel-heading");
-        this._titleSpan = heading.append("span").classed("label label-primary", true).style("margin-left","5px");
+        this._titleSpan = heading.append("span").classed("label label-primary", true).style("margin-left", "5px");
         heading.append("br");
         heading.append("button").classed("glyphicon glyphicon-floppy-save flowbutton", true).on("click", function() {
             me._save();
@@ -28,19 +28,23 @@ define(["util"], function(Util) {
             mode: "python",
             theme: "icecoder"
         });
+
+        this._editor.on("change", function(cm, change) {
+            var code = cm.getValue();
+            me._validate(code);
+        });
     };
 
     Panel.prototype.text = function(data) {
         if (data == undefined) {
             return this._editor.getValue();
         }
-
         this._editor.setValue(data);
     };
 
     Panel.prototype.update = function(node) {
         this._currentNode = node;
-        this._titleSpan.text( node.id() + " : " + node.title());
+        this._titleSpan.text(node.id() + " : " + node.title());
         this.text(node.func());
     };
 
@@ -48,6 +52,28 @@ define(["util"], function(Util) {
         this._currentNode.func(this._editor.getValue());
         this._currentNode.save();
         console.log("node saved!");
+    };
+
+    Panel.prototype._validate = function(code) {
+        var lines = code.split("\n");
+        var func_declare_line = lines[0];
+        var func_pattern = /def(\s)+func\(([\w|\W]*,?)*\):/g;
+        var is_valid_function = func_pattern.test(func_declare_line);
+        var node = this._currentNode;
+
+        if (is_valid_function) {
+            //TODO : validate singature
+            var start_pos = func_declare_line.indexOf("(");
+            var end_pos = func_declare_line.indexOf(")");
+            var parameter = func_declare_line.substr(start_pos + 1, end_pos - start_pos - 1);
+            console.log(parameter);
+            parameters = parameter.split(",");
+            this._currentNode.port().input = [];
+            parameters.map(function(d){
+                node.port().input.push({"name":d});
+            });
+            this._propertyPanel.update(node);
+        }
     };
 
     return Panel;
